050f6e0d4d042d67265be5cfecd6f31bcda9e63e
pouya <pooya.pera@gmail.com>
2025-01-16 13:35:20 +0100

    testing

--- obj/Debug/net9.0/BMECatProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/BMECatProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BMECatProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d154bf02bc56c8dce7b9ce822182f80ee1e22e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a7ed2de78b0017d52ede1840a4cec4dbc27dfcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BMECatProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BMECatProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
